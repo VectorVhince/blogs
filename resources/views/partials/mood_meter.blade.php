50cbbbfc8fbce5c88b8c6c02c01470fa67291a9b
Mau Maun <mau.maun30@gmail.com>
2017-01-26 06:08:02 +0800

    png

--- resources/views/partials/mood_meter.blade.php
+++ resources/views/partials/mood_meter.blade.php
@@ -11,10 +11,10 @@
         </div>
         <div id="moodForm" data-url="{{ route('mood.store',$post->id) }}" data-token="{{ Session::token() }}">
           <ul class="list-inline">
-            <li><button type="button" name="mood" value="happy" class="bgc0 bd0 moodBtn" data-toggle="tooltip" title="Happy"><img src="{{ asset('/img/emoticons/shout.png') }}" class="ht35"> {{ $happy }}</button></li>
-            <li><button type="button" name="mood" value="love" class="bgc0 bd0 moodBtn" data-toggle="tooltip" title="Love"><img src="{{ asset('/img/emoticons/love.png') }}" class="ht35"> {{ $love }}</button></li>
-            <li><button type="button" name="mood" value="shocked" class="bgc0 bd0 moodBtn" data-toggle="tooltip" title="Shocked"><img src="{{ asset('/img/emoticons/startle.png') }}" class="ht35"> {{ $shocked }}</button></li>
-            <li><button type="button" name="mood" value="angry" class="bgc0 bd0 moodBtn" data-toggle="tooltip" title="Angry"><img src="{{ asset('/img/emoticons/anger.png') }}" class="ht35"> {{ $angry }}</button></li>
+            <li><button type="button" name="mood" value="happy" class="bgc0 bd0 moodBtn" data-toggle="tooltip" title="Happy"><img src="{{ asset('/img/emoticons/shout.PNG') }}" class="ht35"> {{ $happy }}</button></li>
+            <li><button type="button" name="mood" value="love" class="bgc0 bd0 moodBtn" data-toggle="tooltip" title="Love"><img src="{{ asset('/img/emoticons/love.PNG') }}" class="ht35"> {{ $love }}</button></li>
+            <li><button type="button" name="mood" value="shocked" class="bgc0 bd0 moodBtn" data-toggle="tooltip" title="Shocked"><img src="{{ asset('/img/emoticons/startle.PNG') }}" class="ht35"> {{ $shocked }}</button></li>
+            <li><button type="button" name="mood" value="angry" class="bgc0 bd0 moodBtn" data-toggle="tooltip" title="Angry"><img src="{{ asset('/img/emoticons/anger.PNG') }}" class="ht35"> {{ $angry }}</button></li>
           </ul>
         </div>
         <div id="moodSuccess" class="dp0">
